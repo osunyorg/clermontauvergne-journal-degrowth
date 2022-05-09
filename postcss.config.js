777e9e4c40b7845b9b3f69c1712570495e7f05df
sebousan <sebousan@gmail.com>
2022-05-09 10:22:23 +0200

    theme

--- postcss.config.js
+++ postcss.config.js
@@ -1,39 +1,72 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    plugins: {
-        '@fullhuman/postcss-purgecss': {
-            content: [
-                './themes/**/*.html',
-                'layouts/**/*.html'
-            ],
-            safelist: {
-                standard: [
-                  'show',
-                  'active',
-                  'modal-backdrop',
-                  /^nav-level-/,
-                  /^is-/,
-                  /^has-/,
-                  /^js-/
-                ],
-                greedy: [
-                  /administrators__/,
-                  /articles__/,
-                  /authors__/,
-                  /categories__/,
-                  /page__/,
-                  /pages__/,
-                  /persons__/,
-                  /posts__/,
-                  /programs__/,
-                  /teachers__/,
-                  /volumes__/
-                ]
-            }
-        },
+/* eslint-disable no-undef */
+var devPlugins = {},
+    productionPlugins = {
         autoprefixer: {},
         cssnano: {
             preset: 'default'
+        },
+        '@fullhuman/postcss-purgecss': {
+            content: [
+                // './hugo_stats.json',
+                './themes/**/*.html',
+                'layouts/**/*.html'
+            ],
+            // defaultExtractor: (content) => {
+            //     let els = JSON.parse(content).htmlElements;
+            //     return els.tags.concat(els.classes, els.ids);
+            // },
+            safelist: {
+                standard: [
+                    'show',
+                    'active',
+                    'collapsed',
+                    /^dropdown/,
+                    /^nav-level-/,
+                    /^splide_/,
+                    /^is-/,
+                    /^has-/,
+                    /^js-/,
+
+                    // Glightbox
+                    'wait-autoplay',
+                    'gfadeIn',
+                    'gfadeOut',
+                    'gslideOutLeft',
+                    'gslideInLeft',
+                    'gslideOutRight',
+                    'gslideInRight',
+                    'gzoomIn',
+                    'gzoomOut'
+                ],
+                deep: [
+                    // Glightbox
+                    /^glightbox/,
+                    /^gslide/,
+                    /^desc-top/,
+                    /^desc-left/,
+                    /^ginlined/,
+                    /^zoomed/,
+                    /^gdesc-/,
+                    /^gabsolute/,
+                    /^grelative/,
+                    /^gloader/,
+                    /^goverlay/,
+                    /^gprev/,
+                    /^gnext/,
+                    /^gclose/,
+                    /^gbtn/,
+                    /^gcontainer/
+                ],
+                greedy: [
+                    /__home/,
+                    /__page/,
+                    /__section/,
+                    /__term/
+                ]
+            }
         }
-    }
+    };
+
+module.exports = {
+    plugins: process.env.HUGO_ENVIRONMENT === 'production' ? productionPlugins : devPlugins
 };
